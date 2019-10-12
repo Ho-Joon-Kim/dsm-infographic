d5a4e3984205233e9960b1597e6ff884af49b650
Yunseo Hwang <yunseo.h68@gmail.com>
2019-10-12 11:18:07 +0900

    [FIX] Header 및 Login 부분 스타일 추가

--- dsm-infographic/views/js/index.js
+++ dsm-infographic/views/js/index.js
@@ -1,10 +1,21 @@
-document.getElementById("loginClose").addEventListener("click", function(){
-    document.getElementById("loginWrap").style.height = "0%";
-    document.getElementById("loginContent").style.display = "none";
-});
+var loginOpenButton = document.getElementById("loginOpen");
+var loginCloseButton = document.getElementById("loginClose");
+var loginBackground = document.getElementById("loginBackground")
+var loginContent = document.getElementById("loginContent")
 
-document.getElementById("loginOpen").addEventListener("click", function(){
-    console.log("Open");
-    document.getElementById("loginWrap").style.height = "100%";
-    document.getElementById("loginContent").style.display = "inline-block";
-});
+loginOpenButton.addEventListener("click", loginOpen);
+loginCloseButton.addEventListener("click", loginClose);
+loginBackground.addEventListener("click", loginClose);
+
+function loginOpen() {
+    loginBackground.style.height = "100%";
+    loginContent.style.display = "inline-block";
+}
+
+function loginClose() {
+    focusEle = document.activeElement;
+    if(focusEle != loginContent) {
+        loginBackground.style.height = "0%";
+        loginContent.style.display = "none";
+    }
+}
