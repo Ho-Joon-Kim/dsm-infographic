ffdc5a481c6098832099a08a1f425439ce27ae45
TitanV <a01098930999@gmail.com>
2019-10-15 19:12:06 +0900

    Save : 저장임 ㅇㅇ

---
+++ dsm-infographic/backend/routes/loginchk.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+var user = read("C:/Users/user/Documents/Git/dsm-infographic/dsm-infographic/backend/public/user.json");
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    userdata = JSON.parse(user);
+    res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
