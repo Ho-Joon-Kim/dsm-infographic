4b110db82a7594bc8b0703f5ae200d2b5fc1be68
TitanV <a01098930999@gmail.com>
2019-10-17 15:47:43 +0900

    ADD : 로그인 api 추가
    아마 될거임 
    아이디 : 1111
    비밀번호 : helloworld
    로그인 완료시 "실험체1" 반환

--- dsm-infographic/backend/app.js
+++ dsm-infographic/backend/app.js
@@ -3,24 +3,32 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
+
+var mainRouter = require('./routes/');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var infoRouter = require('./routes/info');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', mainRouter);
+app.use('/index', indexRouter);
+app.use('/login', loginRouter);
+app.use('/info', infoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ dsm-infographic/backend/routes/login.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var user = read("C:/Users/user/Documents/Git/dsm-infographic/dsm-infographic/backend/public/user.json");
+
+
+
+router.post('/', function(req, res, next) {
+    var user_id = req.body.id;
+    var user_pswd= req.body.pswd;
+
+    if (user_id in user) 
+    {
+        if (user.user_id.pswd == user_pswd)
+        {
+            res.send(user.user_id.name)
+        }
+
+        else
+        {
+            res.send("error")
+        }
+    }
+
+    else
+    {
+        res.send("error")
+    }
+});
+
+module.exports = router;
