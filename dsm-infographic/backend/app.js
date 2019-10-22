fea358ae90530c2b77e4233f9ba4b300656d5255
TitanV <a01098930999@gmail.com>
2019-10-22 23:47:27 +0900

    Add : 설문조사 결과저장 api 완료

--- dsm-infographic/backend/app.js
+++ dsm-infographic/backend/app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var infoRouter = require('./routes/info');
 var descRouter = require('./routes/desc');
+var serveyRouter = require('./routes/servey');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/api/index', indexRouter);
 app.use('/api/login', loginRouter);
 app.use('/api/info', infoRouter);
 app.use('/api/desc', descRouter);
+app.use('/api/servey', serveyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- dsm-infographic/backend/routes/survey.js
+++ dsm-infographic/backend/routes/survey.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var user = require("../public/user.json");
+var survey = require("../public/survey.json");
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    var q1_answer = req.body.q1;
+    var q2_answer = req.body.q2;
+    var infoNum = req.body.infoNum;
+    var id = req.body.id;
+
+    survey[infoNum]["q1"] = survey[infoNum]["q1"] + q1_answer;
+    survey[infoNum]["q2"] = survey[infoNum]["q2"] + q2_answer;
+
+    user[id][info_s] = user[id][info_s] + str(infoNum)
+        
+
+
+    res.send({output : "done"});
 });
 
 module.exports = router;
