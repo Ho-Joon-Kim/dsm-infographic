45d412adefcb0ac28503ac2cead551e704e96e7b
TitanV <a01098930999@gmail.com>
2019-10-24 22:18:47 +0900

    Fix : js파일 코드내 위치 수정

--- dsm-infographic/backend/routes/info/img.js
+++ dsm-infographic/backend/routes/info/img.js
@@ -6,19 +6,21 @@ router.get('/', function(req, res, next) {
     var info_id = req.query.imgid
 
 
-    var filename = `../../public/info/${info_id}.png`;
+    var filename = `public/info/${ info_id}.PNG`;
     fs.readFile(filename,              //파일 읽기
         function (err, data)
         {
-            res.writeHead(200, { "Context-Type": "image/png" });//보낼 헤더를 만듬
+            if(err) {
+                console.log(err);
+            } else {
+                
+            res.writeHead(res.statusCode.toString(), { "Context-Type": "image/png" });//보낼 헤더를 만듬
             res.write(data);   //본문을 만들고
             res.end();  //클라이언트에게 응답을 전송한다
 
+            }
         }
     );
-
-
-
 
 });
 
