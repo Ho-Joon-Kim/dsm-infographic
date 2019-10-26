9afd4da3709f0307a370954c83f7502f9528c0de
Yunseo Hwang <yunseo.h68@gmail.com>
2019-10-26 23:40:19 +0900

    Feat : DsmSciFooter 컴포넌트 개발 완료
    사이트 목적, Copyright 등의 정보를 담고 있는 Footer 컴포넌트의 개발완료
    
    Resolves: #20

--- dsm-infographic/frontend/src/components/DsmSciFooter/DsmSciFooter.vue
+++ dsm-infographic/frontend/src/components/DsmSciFooter/DsmSciFooter.vue
@@ -1,15 +1,33 @@
 <template>
-  <p class="copyright">
-    &copy; 2019
-    <a href="https://cafe.naver.com/dsmhssci2019.cafe">DSMHS_SCI</a>. Made By
-    <a href="https://github.com/Ho-Joon-Kim">김호준</a>,
-    <a href="https://github.com/yunseo-h68">황윤서</a>.
-  </p>
+  <div id="dsmSciFooter" class="dsmsci-footer">
+    <p class="copyright">
+      &copy; {{ computedYear }}
+      <a :href="copyrightLink">대덕소프트웨어마이스터고등학교 과학과.</a>
+    </p>
+    <p class="notice">
+      이 사이트는 대덕SW마이스터고등학교 과학과에서 1학년을 대상으로 실시한 기후 인포그래픽 평가를 위한 사이트입니다.
+    </p>
+    <p class="notice">
+      본 사이트에 대한 소스는 <a :href="srcLink">이곳</a>에서 확인할 수 있습니다.
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'dsmsci-footer',
+  data() {
+    return {
+      copyrightLink: 'https://cafe.naver.com/dsmhssci2019.cafe',
+      srcLink: 'https://github.com/Ho-Joon-Kim/dsm-infographic',
+    };
+  },
+  computed: {
+    computedYear() {
+      const date = new Date();
+      return date.getUTCFullYear();
+    },
+  },
 };
 </script>
 
