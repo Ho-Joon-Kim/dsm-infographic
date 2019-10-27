5fcdfc2fb53d9ecfd04691af032e2e0575a8c43e
Yunseo Hwang <yunseo.h68@gmail.com>
2019-10-27 15:06:58 +0900

    Add : Admin 권한이 없을 때, /admin으로 접속하지 못하도록 beforeEnter를 설정
    beforeEnter로 Admin인지 체크하고 아니라면 /로 이동시키도록 함

--- dsm-infographic/frontend/src/router.js
+++ dsm-infographic/frontend/src/router.js
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import Admin from './views/Admin.vue';
+import store from '@/vuex/store';
 
 Vue.use(Router);
+
+const requireAdmin = () => (from, to, next) => {
+  if (store.getters.getAdminCheck) return next(); // isAuth === true면 페이지 이동
+  return next('/'); // isAuth === false면 다시 로그인 화면으로 이동
+};
 
 export default new Router({
   mode: 'history',
@@ -18,6 +24,7 @@ export default new Router({
       path: '/admin',
       name: 'admin',
       component: Admin,
+      beforeEnter: requireAdmin(),
     },
   ],
 });
